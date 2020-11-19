dc43d3deafdf2d9d2eb660dbb30ebe74cbaf5d2e
144026 <44112701+144026@users.noreply.github.com>
2020-11-19 18:19:42 +0800

    rcv.0.4.2

--- 3Src/hoprcv.m
+++ 3Src/hoprcv.m
@@ -13,7 +13,7 @@ function ret = hoprcv()
 	
 	% Transmit and Receive using MATLAB libiio
 	% System Object Configuration
-	s = iio_sys_obj_matlab; % MATLAB libiio Constructor
+	global s = iio_sys_obj_matlab; % MATLAB libiio Constructor
 	s.ip_address = ip;
 	s.dev_name = 'ad9361';
 	s.in_ch_no = 2;
@@ -23,8 +23,8 @@ function ret = hoprcv()
 	
 	s = s.setupImpl();
 	
-	input = cell(1, s.in_ch_no + length(s.iio_dev_cfg.cfg_ch));
-	output = cell(1, s.out_ch_no + length(s.iio_dev_cfg.mon_ch));
+	global input = cell(1, s.in_ch_no + length(s.iio_dev_cfg.cfg_ch));
+	global output = cell(1, s.out_ch_no + length(s.iio_dev_cfg.mon_ch));
 	
 	% Set the attributes of AD9361
 	input{s.getInChannel('RX_LO_FREQ')} = 2e9;
@@ -51,15 +51,15 @@ function ret = hoprcv()
 		I=output{1};
 		Q=output{2};
 		rxdata=I+1i*Q;
-	   	[crc_res,urmsg]=my_bpsk_rx_func(rxdata);
-		if crc_res == 1 && urmsg(1) == hex2dec('ad') && urmsg(2) == hex2dec('13')
-			switch urmsg(3)
+	   	[crc_res,rUmsg]=my_bpsk_rx_func(rxdata);
+		if crc_res == 1 && rUmsg(1) == hex2dec('ad') && rUmsg(2) == hex2dec('13')
+			switch rUmsg(3)
 				case 0 %Hopping SYN
 					;
 				case 1 %Hopping ACK
 					;
 				case 2 %File
-					TotalDataCount = ubytes2word(urmsg(5:8));
+					TotalDataCount = ubytes2word(rUmsg(5:8));
 				case 3 %File Secondary
                     if TotalDataCount == 0
                         continue;
@@ -68,8 +68,8 @@ function ret = hoprcv()
 					if ct == TotalDataCount
 						break;
 					end
-					DataCount = uint32(urmsg(4));
-					bf = [bf,urmsg(5:DataCount+4)];
+					DataCount = uint32(rUmsg(4));
+					bf = [bf,rUmsg(5:DataCount+4)];
 					ct = ct + DataCount;
 			end
 		else
@@ -91,8 +91,31 @@ function ret = hoprcv()
 end
 
 
-%% utility functions
-function Dispatcher(umsg)
+%% functions
+
+%% APIs
+function play()
+	global s;
+	global input;
+	global output;
+
+	seq=uint32(0);
+	while true
+		output = readRxData(s);
+		I=output{1};
+		Q=output{2};
+		rxdata=I+1i*Q;
+	   	[crc_res,rUmsg]=my_bpsk_rx_func(rxdata);
+		if crc_res==1 && rUmsg(5:8) == seq+1
+			seq = seq + 1;
+			rcvDispatcher(rUmsg);
+		else
+			sUmsg=PackUmsg(rUmsg(3),rUmsg(4),seq,rUmsg(9:60));
+		end
+	end
+end
+
+function rcvDispatcher(umsg)
 	%% Type definition
 	HOPPING_SYN = uint8(0);
 	HOPPING_ACK = uint8(1);
@@ -113,21 +136,21 @@ function Dispatcher(umsg)
 end
 
 function CompleteFile(umsg)
-	main.TotalDataCount = ubytes2word(urmsg(5:8));
+	main.TotalDataCount = ubytes2word(rUmsg(5:8));
 	sendACK
 end
 
 function CompleteFileSecondary(umsg)
 
 
-
-function umsg = packdata(Type,DataCount,Data)
+function umsg = PackUmsg(Type,DataCount,Seq,Data)
 	Magic = [uint8(hex2dec('ad')), uint8(hex2dec('13'))];
 	Type = uint8(Type);
 	DataCount = uint8(DataCount);
-	Data = [uint8(Data), uint8(zeros(1,56))];
-	Data = Data(1:56);
-	umsg = [Magic,Type,DataCount,Data];
+	Seq=word2ubytes(uint32(Seq));
+	Data = [uint8(Data), uint8(zeros(1,52))];
+	Data = Data(1:52);
+	umsg = [Magic,Type,DataCount,Seq,Data];
 end
 
 function sendumsg(s,input,umsg)
@@ -138,7 +161,19 @@ function sendumsg(s,input,umsg)
     input{2} = imag(txdata);
 	writeTxData(s,input);	 
 end
+function SendUmsg(umsg)
+	global s;
+	global input;
+	txdata = my_bpsk_tx_func(umsg);
+	txdata = round(txdata .* 2^14);
+	txdata=repmat(txdata, 8,1);
+	input{1} = real(txdata);
+    input{2} = imag(txdata);
+	writeTxData(s,input);	 
+end
 
+
+%% utilities
 function ret = word2ubytes(uword)
 	%% little
 	uword = uint32(uword);
